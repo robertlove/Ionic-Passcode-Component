d93153078052ae55f0e36c53157de0dc9dc68ac8
Robert Love <robertjohnlove@gmail.com>
2017-08-05 19:44:29 +1000

    Component docs and screenshot

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Ionic-Passcode-Component
+# Ionic Passcode Component
+
+![Ionic Passcode Component](screenshot.png)
+
+## Installation
+
+## Basic Usage
+
+```html
+<passcode></passcode>
+```
+
+## Configuration
+
+The Ionic Passcode Component is easily configurable via HTML attributes.
+
+Name      | Type    | Default | Description
+--------- | ------- | ------- | -----------
+backspace | boolean | true    | Whether to show the backspace button
+clear     | boolean | true    | Whether to show the clear button
+letters   | boolean | true    | Whether to show letters on the number buttons
+name      | string  |         | The name attribute value of the hidden input element
+size      | number  | 4       | The size of the passcode. Must be a number greater than 0
+value     | number  |         | The current value of the passcode and the value attribute value of the hidden input element
+
+## Events
+
+Name        | Description
+----------- | -----------
+cleared     | Event Emitted whenever the passcode has been cleared
+completed   | Event Emitted whenever the passcode has been completed
+decremented | Event Emitted whenever the passcode has been decremented
+incremented | Event Emitted whenever the passcode has been incremented
---
+++ passcode/passcode.ts
@@ -0,0 +1,148 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'passcode',
+  templateUrl: 'passcode.html'
+})
+
+export class PasscodeComponent {
+
+  /**
+   * @input {any} Whether to show the backspace button.
+   */
+  @Input('backspace') showBackspaceButton: any = 'true';
+
+  /**
+   * @input {any} Whether to show the clear button.
+   */
+  @Input('clear') showClearButton: any = 'true';
+
+  /**
+   * @input {any} Whether to show letters on the number buttons.
+   */
+  @Input('letters') showLetters: any = 'true';
+
+  /**
+   * @input {string} The name attribute value of the hidden input element.
+   */
+  @Input('name') name: string = '';
+
+  /**
+   * @input {any} The size of the passcode. Must be a number greater than 0.
+   */
+  @Input('size') size: any = 4;
+
+  /**
+   * @input {string} The current value of the passcode and the value attribute value of the hidden input element.
+   */
+  @Input('value') value: string = '';
+
+  /**
+   * @output {EventEmitter} Event Emitted whenever the passcode has been cleared.
+   */
+  @Output() cleared: EventEmitter<any> = new EventEmitter();
+
+  /**
+   * @output {EventEmitter} Event Emitted whenever the passcode has been completed.
+   */
+  @Output() completed: EventEmitter<any> = new EventEmitter();
+
+  /**
+   * @output {EventEmitter} Event Emitted whenever the passcode has been decremented.
+   */
+  @Output() decremented: EventEmitter<any> = new EventEmitter();
+
+  /**
+   * @output {EventEmitter} Event Emitted whenever the passcode has been incremented.
+   */
+  @Output() incremented: EventEmitter<any> = new EventEmitter();
+
+  /**
+   * @var {boolean} Whether the passcode is complete.
+   */
+  isComplete: boolean = false;
+
+  /**
+   * @var {number} The current length of the passcode.
+   */
+  length: number = 0;
+
+  constructor() {}
+
+  ngAfterViewInit() {
+
+    // Ensure size is a number greater than 0. If it isn't, set it to 4.
+    this.size = Number(this.size);
+    if (isNaN(this.size) || this.size <= 0) {
+      this.size = 4;
+    }
+
+    // Ensure the value is not longer than the size.
+    this.value = this.value.substr(0, this.size);
+
+    // Store the current length of the passcode for comparison.
+    this.length = this.value.length;
+
+    // Strings to booleans
+    this.showBackspaceButton = (this.showBackspaceButton == 'true') ? true : false;
+    this.showClearButton = (this.showClearButton == 'true') ? true : false;
+    this.showLetters = (this.showLetters == 'true') ? true : false;
+
+    // Run the check.
+    this.check();
+  }
+
+  /**
+  * Check if the passcode is complete.
+  */
+  check(): void {
+    if (this.length >= this.size) {
+      this.isComplete = true;
+      this.completed.emit(this);
+    }
+  }
+
+  /**
+  * Clear the passcode completely.
+  */
+  clear(): void {
+    this.length = 0;
+    this.value = '';
+    this.isComplete = false;
+    this.cleared.emit(this);
+  }
+
+  /**
+  * Decrement the passcode by 1.
+  */
+  decrement(): void {
+    if (this.length > 0) {
+      this.value = this.value.slice(0, -1);
+      this.length = this.value.length;
+      this.isComplete = false;
+      this.decremented.emit(this);
+    }
+  }
+
+  /**
+  * Increment the passcode with 'number'.
+  */
+  increment(number): void {
+    if (this.length < this.size) {
+      this.value += number;
+      this.length = this.value.length;
+      this.incremented.emit(this);
+      this.check();
+    }
+  }
+
+  /**
+  * Get an array of length 'length'.
+  */
+  range(length): any {
+    return Array.from({
+      length: length
+    });
+  }
+
+}
